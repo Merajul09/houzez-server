7ce1f5d3324d5163cb4ba742ef55c40aac5f4a83
Merajul09 <merajulhasan09@gmail.com>
2024-06-21 18:10:01 +0600

    added some apis

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const corsOption = {
+    origin: [
+        'http://localhost:5173',
+    ],
+    Credentials: true,
+    optionSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}
+
 // middleware
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log(token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wov5hm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,13 +52,213 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+        const usersCollection = client.db("houzez").collection("users");
+        const propertyCollection = client.db("houzez").collection("properties");
+        const bookingsCollection = client.db("houzez").collection("bookings");
+
+        const verifyAdmin = async (req, res, next) => {
+            const user = req.user
+            const query = { email: user?.email }
+            const result = await usersCollection.findOne(query)
+            console.log(result?.role)
+            if (!result || result?.role !== 'admin')
+                return res.status(401).send({ message: 'unauthorized access!!' })
+
+            next()
+        }
+        // verify agent middleware
+        const verifyAgent = async (req, res, next) => {
+            console.log('hello')
+            const user = req.user
+            const query = { email: user?.email }
+            const result = await usersCollection.findOne(query)
+            console.log(result?.role)
+            if (!result || result?.role !== 'agent') {
+                return res.status(401).send({ message: 'unauthorized access!!' })
+            }
+
+            next()
+        }
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+
+        // save a user data in db
+        app.put('/user', async (req, res) => {
+            const user = req.body
+            const query = { email: user?.email }
+            const isExist = await usersCollection.findOne(query)
+            if (isExist) {
+                if (user.status === 'Requested') {
+                    const result = await usersCollection.updateOne(query, {
+                        $set: { status: user?.status },
+                    })
+                    return res.send(result)
+                } else {
+                    return res.send(isExist)
+                }
+            }
+            // save user for the first time
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    ...user,
+                    timestamp: Date.now(),
+                },
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        })
+        // get a user info by email from db
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const result = await usersCollection.findOne({ email })
+            res.send(result)
+        })
+        // get all users data from db
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+        //update a user role
+        app.patch('/users/update/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email }
+            const updateDoc = {
+                $set: { ...user, timestamp: Date.now() },
+            }
+            const result = await usersCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+        // Get all rooms from db
+        app.get('/properties', async (req, res) => {
+            const category = req.query.category
+            console.log(category)
+            let query = {}
+            if (category && category !== 'null') query = { category }
+            const result = await propertyCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Save a room data in db
+        app.post('/property', async (req, res) => {
+            const propertyData = req.body
+            const result = await propertyCollection.insertOne(propertyData)
+            res.send(result)
+        })
+        // delete a room
+        app.delete('/property/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await propertyCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // Get a single property data from db using _id
+        app.get('/property/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await propertyCollection.findOne(query)
+            res.send(result)
+        })
+        // update Property data
+        app.put('/property/update/:id', async (req, res) => {
+            const id = req.params.id
+            const propertyData = req.body
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: propertyData,
+            }
+            const result = await propertyCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+
+        // update Property Status
+        app.patch('/property/status/:id', async (req, res) => {
+            const id = req.params.id
+            const status = req.body.status
+            // change property availability status
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: { booked: status },
+            }
+            const result = await propertyCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+
+        // get all Property for agent
+        app.get(
+            '/my-listings/:email',
+            async (req, res) => {
+                const email = req.params.email
+
+                let query = { 'agent.email': email }
+                const result = await propertyCollection.find(query).toArray()
+                res.send(result)
+            }
+        )
+
+        // get all booking for a guest
+        app.get('/my-bookings/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { 'guest.email': email }
+            const result = await bookingsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // get all booking for an Agent
+        app.get(
+            '/manage-bookings/:email',
+            async (req, res) => {
+                const email = req.params.email
+                const query = { 'agent.email': email }
+                const result = await bookingsCollection.find(query).toArray()
+                res.send(result)
+            }
+        )
+
+        // delete a booking
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // Clear token on logout
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+            } catch (err) {
+                res.status(500).send(err)
+            }
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
